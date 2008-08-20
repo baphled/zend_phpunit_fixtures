354e96e1454e23efeaa51f9cd619b07e3d5bbb75
yomi <yomi@1f56db89-a82d-46e4-8122-373cd12a268a>
2008-08-20 12:39:14 +0000

    Added config directory for our db settings, also added settings.ini, which needs changing. Added tests lib directory, so that we can move Module_PHPUnit_Framework_TestCase. Added TestConfigSettings, which builds our DB connection & config settings. Added Unit directory which will be used to house our unit test cases.
    git-svn-id: http://svn.ibetx.com/svn/intranet@2 1f56db89-a82d-46e4-8122-373cd12a268a

---
+++ tests/TestConfigSettings.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * TestConfigSettings
+ * 
+ * Basic little class that handles all our config settings, we basically
+ * call each method when we need setup a particular setting.
+ * 
+ * @author Yomi (baphled) Akindayini 2008
+ * @version $Id$
+ * @copyright 2008
+ * @package IntraBetxTestSuite
+ *
+ * Date:19/08/08
+ * Made getParam private, seeing as it should
+ * only be used internally.
+ */
+require_once 'Zend/Loader.php';
+Zend_Loader::registerAutoload();
+
+class TestConfigSettings {
+	
+	static protected $_config;
+	
+    static function setUpConfig($env = 'development') {
+        $root = realpath(dirname(__FILE__) . '/../');
+        $configFile = $root .'/configs/settings.ini';               // smell?
+        self::$_config = new Zend_Config_Ini( $configFile, $env);
+        Zend_Registry::set('config',self::$_config);
+    }
+    
+    private static function _getDBParams($config) {
+        $params = array( 'host'     => $config->database->hostname,
+                 'username' => $config->database->username,
+                 'password' => $config->database->password,
+                 'dbname'   => $config->database->database);
+        return $params;
+    }
+    
+    static function setUpDBAdapter() {
+    	$config = self::$_config;
+    	if(null === $config->type ) {
+    		$adapter = 'PDO_MYSQL';
+    	}
+        $params = self::_getDBParams($config);
+        $db = Zend_Db::factory($adapter, $params);
+        $db->setFetchMode(Zend_Db::FETCH_OBJ);
+        Zend_Db_Table::setDefaultAdapter($db);
+        Zend_Registry::set('db',$db);
+    }
+}
