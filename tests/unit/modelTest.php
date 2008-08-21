37b9dea511ffdadf6883553d0561668fe9ba90cd
yomi <yomi@1f56db89-a82d-46e4-8122-373cd12a268a>
2008-08-21 09:48:59 +0000

    Test test, used to make sure TDD environment is setup.
    git-svn-id: http://svn.ibetx.com/svn/intranet@16 1f56db89-a82d-46e4-8122-373cd12a268a

---
+++ tests/unit/modelTest.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ *
+ * modelTestCase
+ * 
+ * @author Yomi (baphled) Akindayini 2008
+ * @version $Id$
+ * @copyright 2008
+ * @package 
+ * @subpackage TestSuite
+ *
+ * Date: Aug 21, 2008
+ * 
+ */
+
+set_include_path ( '.' . PATH_SEPARATOR .realpath(dirname(__FILE__) .'/../libs/') . PATH_SEPARATOR. dirname ( __FILE__ ) . '/../../library/' . PATH_SEPARATOR .dirname ( __FILE__ ) . '/../../library/' . PATH_SEPARATOR. dirname ( __FILE__ ) . '/../../application/default/models/' . PATH_SEPARATOR . get_include_path () );
+
+require_once 'Zend/Loader.php';
+Zend_Loader::registerAutoload ();
+
+class modelTest extends Module_PHPUnit_Framework_TestCase {
+	
+	private $_model;
+	
+	public function __construct() {
+		$this->setName ( 'modelTest Case' );
+	}
+	
+	public function setUp() {
+		$this->_setUpConfig ();
+		parent::setUp ();
+	}
+	
+	public function tearDown() {
+		$this->_model = null;
+		parent::tearDown ();
+	}
+	
+	function testAssert() {
+		$this->assertEquals(1,1);
+	}
+}
