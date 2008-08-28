853ea9fbf78f33eb4ce766c6465cb474e5503abd
yomi <yomi@1f56db89-a82d-46e4-8122-373cd12a268a>
2008-08-28 10:24:08 +0000

    Gutted implementation along with testcase, so that we can test using our user api as well as seperating our DB implementation.
    git-svn-id: http://svn.ibetx.com/svn/intranet@37 1f56db89-a82d-46e4-8122-373cd12a268a

--- application/features/models/Features.php
+++ application/features/models/Features.php
@@ -3,59 +3,13 @@
 /**
  * Features
  *  
- * @author Nadjaha (ibetxadmin) Wohedally 2008
+ * @author Yomi (baphled) Akindayini 2008
  * @version $Id$
  * @copyright ibetX Ltd2008
  * @package
  *  
  */
 
-require_once 'Zend/Db/Table/Abstract.php';
-
-class Features extends Zend_Db_Table_Abstract {
-	/**
-	 * The default table name 
-	 */
-	protected $_name = 'features';
+class Features {
 	
-	function addNewFeature($newFeature){		
-		if(!is_array($newFeature)) {
-			throw new ErrorException('Must be a valid array');
-		}
-		if(null === $newFeature['title']){
-			throw new ErrorException('Feature must have a title.');
-		}
-		else{
-			return (int)$this->insert($newFeature);
-		}
-	}
-	
-	function _featureExists($feature) {
-		$db = Zend_Registry::get('db');
-		$select = $db->select()
-					->from('features',array('title'))
-					->where('title = ?',$feature['title']);
-		$stmt = $db->query($select);
-		$result = $stmt->fetchAll();
-		if(count($result)) {
-			return true;		
-		}
-		else {
-			return false;
-		}
-	}
-	
-	function listFeatures(){
-		$result = $this->fetchAll()->toArray();
-		if(0 === count($result)){
-			throw new ErrorException('Feature list empty.');			
-		}
-		return $result;
-	}
-	
-	function viewFeatureById($id){
-		$result = $this->find($id);
-		return $result->current()->toArray();
-	}
-
 }
