6e4d1372597d76edda01f49511e2e4f14b123488
yomi <yomi@1f56db89-a82d-46e4-8122-373cd12a268a>
2008-09-03 19:49:14 +0000

    Improved general file format.
    git-svn-id: http://svn.ibetx.com/svn/intranet@93 1f56db89-a82d-46e4-8122-373cd12a268a

--- tests/unit/PHPUnitFixturesUnitSuite.php
+++ tests/unit/PHPUnitFixturesUnitSuite.php
@@ -8,9 +8,12 @@
  * @copyright 2008
  * @package TestSuite
  *
+ * $LastChangedBy$
+ * 
  */
 
-set_include_path ( '.' . PATH_SEPARATOR .realpath(dirname(__FILE__) .'/../libs/') . PATH_SEPARATOR. dirname ( __FILE__ ) . '/../../library/' . PATH_SEPARATOR .dirname ( __FILE__ ) . '/../../library/' . PATH_SEPARATOR. dirname ( __FILE__ ) . '/../../application/default/models/' . PATH_SEPARATOR . get_include_path () );
+set_include_path ( '.' . PATH_SEPARATOR .realpath(dirname(__FILE__) .'/../libs/') 
+                       .PATH_SEPARATOR . get_include_path () );
 
 require_once 'Zend/Loader.php';
 Zend_Loader::registerAutoload ();
