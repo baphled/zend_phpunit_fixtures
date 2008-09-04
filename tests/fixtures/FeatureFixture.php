ea3e4b1bb1922637405256e730ec1c2e541fd593
nadjaha <nadjaha@1f56db89-a82d-46e4-8122-373cd12a268a>
2008-09-04 12:59:50 +0000

    Fixtures for our features class.
    git-svn-id: http://svn.ibetx.com/svn/intranet@103 1f56db89-a82d-46e4-8122-373cd12a268a

---
+++ tests/fixtures/FeatureFixture.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @author Yomi (baphled) Akindayini 2008
+ * @version $Id: Features.php 41 2008-08-28 15:48:50Z nadjaha $
+ * @copyright ibetX Ltd2008
+ * @package
+ * 
+ * Features fixtures class used to store and handle our test data.
+ * 
+ */
+
+require_once 'Zend/Loader.php';
+Zend_Loader::registerAutoload ();
+
+class FeatureFixture extends PHPUnit_Fixture {
+	public $_table = 'features';
+	
+	public $_fields = array(
+					 'id' => array('type' => 'integer', 'length'=>10, 'key' => 'primary'),
+					 'userid' => array('type' => 'integer', 'length'=>10, 'null', FALSE),
+				 	 'title' => array('type' =>'string', 'length' => 255, 'null', FALSE),
+					 'description' => array('type'=>'string','length'=>255, 'null', FALSE)
+					  );
+	
+	public $_testData = array(
+						array('id' => 1, 'userid' => 1,'title' => 'new feature','description' => 'To test a new feature'),
+						array('id' => 2, 'userid' => 23,'title' => 'anuva feature','description' => 'feature description'),
+						array('id' => 3, 'userid'=> 13,'title' => 'second feature','description' => 'second feature')
+						);
+}
+?>
