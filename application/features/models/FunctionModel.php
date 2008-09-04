873de854ea8745bcfb381beafa84c9ef535e6717
nadjaha <nadjaha@1f56db89-a82d-46e4-8122-373cd12a268a>
2008-09-04 14:26:50 +0000

    Added to be used with Function
    git-svn-id: http://svn.ibetx.com/svn/intranet@108 1f56db89-a82d-46e4-8122-373cd12a268a

---
+++ application/features/models/FunctionModel.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * FunctionModel
+ *  
+ * @author ibetxadmin
+ * @version 
+ */
+
+require_once 'Zend/Db/Table/Abstract.php';
+
+class FunctionModel extends Zend_Db_Table_Abstract {
+	/**
+	 * The default table name 
+	 */
+	protected $_name = 'function';
+
+}
