b882239fc56e2cda8971529d560b647ad868c12c
yomi <yomi@1f56db89-a82d-46e4-8122-373cd12a268a>
2008-09-05 18:01:43 +0000

    Cloned invalid data type structure from FixturesManagers help function, will remove that and use this from now.
    git-svn-id: http://svn.ibetx.com/svn/intranet@131 1f56db89-a82d-46e4-8122-373cd12a268a

--- tests/fixtures/InvalidFieldTypeFixture.php
+++ tests/fixtures/InvalidFieldTypeFixture.php
@@ -12,8 +12,12 @@
 class InvalidFieldTypeFixture extends PHPUnit_Fixture {
 	public $_table = 'snooker';
 	
-	public $_fields = array(
-	        'id' => array('type' => 'integer', 'length' => 10, 'key' => 'primary'),
-            'apple_id' => array('tipe' => 'integer', 'length' => 10, 'null' => true)
-	       );
+	public $_fields  = array(
+            'id' => array('type' => 'integer', 'key' => 'primary'),
+            'parent_id' => array('typed' => 'integer', 'length' => 10, 'null' => true),
+            'model' => array('type' => 'strong', 'length' => 255, 'default' => ''),
+            'alias' => array('type' => 'string', 'length' => 255, 'default' => ''),
+            'lft' => array('type' => 'integer', 'length' => 10, 'null' => true),
+            'rght' => array('type' => 'integer', 'length' => 10, 'null' => true)
+        );
 }
