9eb1602d400b68865f596e2e1ea40576de2f53df
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-04 17:01:52 +0000

    Tests where failing for some weird reason, have now got them working again.

--- tests/libs/TestHelper.php
+++ tests/libs/TestHelper.php
@@ -1,18 +1,10 @@
-<?php 
-/**
- * @author Yomi (baphled) Akindayini 2008
- * @version $Id$
- * @copyright 2008
- * @package Zend_PHPUnit_Scaffolding
- * @subpackage TestHelper
- * 
- */
+<?php
 set_include_path('.' . PATH_SEPARATOR .
 				dirname(__FILE__) .  PATH_SEPARATOR .
-				realpath(dirname(__FILE__) . '/../libs' . PATH_SEPARATOR) .
-				realpath(dirname(__FILE__) . '/../fixtures' . PATH_SEPARATOR) .
-				realpath(dirname(__FILE__) . '/../../application/default/models/' . PATH_SEPARATOR) .
-				realpath(dirname(__FILE__) . '/../../library/' . PATH_SEPARATOR) .
+				dirname(__FILE__) . '/../fixtures' . PATH_SEPARATOR .
+				dirname(__FILE__) . '/../../application/default/models/' . PATH_SEPARATOR .
+				dirname(__FILE__) . '/../../library/' . PATH_SEPARATOR .
+				dirname(__FILE__) . '/../../application/features/models/' . PATH_SEPARATOR . 
 				get_include_path());
 require_once 'Zend/Loader.php';
 Zend_Loader::registerAutoload();
---
+++ tests/unit/IntranetTestSuite.php
@@ -0,0 +1,32 @@
+<?php
+require_once dirname(__FILE__) .'/../libs/TestHelper.php';
+require_once 'phpunit_fixture/PHPUnitFixturesUnitSuite.php';
+
+/**
+ * Static test suite.
+ * @author Yomi (baphled) Akindayini <yomi@boodah.net>
+ * @version $Id: IntranetTestSuite.php 230 2008-10-01 11:53:33Z yomi $
+ * @package Zend_PHPUnit_Scaffolding
+ * 
+ * $LastChangedBy: yomi $
+ */
+class IntranetTestSuite extends PHPUnit_Framework_TestSuite {
+	
+	/**
+	 * Constructs the test suite handler.
+	 */
+	public function __construct() {
+		$this->setName ( 'IntranetTestSuite' );
+		
+		$this->addTestSuite ( 'PHPUnitFixturesUnitSuite' );
+	
+	}
+	
+	/**
+	 * Creates the suite.
+	 */
+	public static function suite() {
+		return new self ( );
+	}
+}
+
