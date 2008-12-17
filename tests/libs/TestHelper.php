9f0acce950ba9044af5621bbb6ca17d4ba79af36
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-17 15:26:18 +0000

    Added unit directory within the tests dir, so that files directly under tests/unit are found.

--- tests/libs/TestHelper.php
+++ tests/libs/TestHelper.php
@@ -22,6 +22,7 @@ set_include_path('.' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/../../init/' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/../../library/' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/../../application/' . PATH_SEPARATOR .
+		dirname(__FILE__) . '/../unit/' . PATH_SEPARATOR .
 	get_include_path());
 require_once 'Zend/Loader.php';
 Zend_Loader::registerAutoload();
