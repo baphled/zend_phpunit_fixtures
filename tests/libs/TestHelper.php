67cbc03119369bb59250fcabefd47a32690ce93b
Yomi (baphled boodah) Akindayini <baphled@boodah.net>
2008-12-17 14:28:13 +0000

    Added include path to the default ZF application directory

--- tests/libs/TestHelper.php
+++ tests/libs/TestHelper.php
@@ -21,6 +21,7 @@ set_include_path('.' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/../../application/default/models/' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/../../init/' . PATH_SEPARATOR .
 	dirname(__FILE__) . '/../../library/' . PATH_SEPARATOR .
+	dirname(__FILE__) . '/../../application/' . PATH_SEPARATOR .
 	get_include_path());
 require_once 'Zend/Loader.php';
 Zend_Loader::registerAutoload();
